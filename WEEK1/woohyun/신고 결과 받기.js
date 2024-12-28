a6b82b82fd4ddcbc5d5215af3dccad77532157c2
DDOLDDOL <doyt@naver.com>
2024-12-28 15:18:57 +0900

    refactor: Refactor solution code

--- WEEK1/woohyun/신고 결과 받기.js
+++ WEEK1/woohyun/신고 결과 받기.js
@@ -12,20 +12,23 @@ function solution(id_list, report, k) {
     
 
     for (_report of report) {
-        // 신고 내역 리스트 
+        // 신고 내역 리스트 split (신고자-피신고자)
         let splitted = _report.split(" ");
         let reporter = splitted[0];
         let reportee = splitted[1];
         
+        // 신고자 맵에 중복 검사 후 저장
         if(!reporteeMap[reporter].includes(reportee)) {
             reporteeMap[reporter].push(reportee);
         }
         
+        // 피신고자 맵에 중복 검사 후 저장
         if(!reporterMap[reportee].includes(reporter)) {
             reporterMap[reportee].push(reporter);
         }
     }
     
+    // 신고자 맵에서 피신고자 리스트를 탐색한 뒤 신고당한 횟수 비교 후 정답 출력
     for (id of id_list) {
         var count = 0;
         
